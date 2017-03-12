3ecb906d183b4f1a36f8445ef3d4d28a56c6f230
Ning Yang <csyangning@gmail.com>
2017-03-12 12:20:44 -0700

    init

---
+++ main.go
@@ -0,0 +1,47 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"net"
+	"os"
+)
+
+var addr = flag.String("addr", ":1818", "learn to address:port")
+
+func main() {
+	flag.Parse()
+
+	listener, err := net.Listen("tcp", *addr)
+	if err != nil {
+		fmt.Println("Error on initializing listener:", err)
+		os.Exit(1)
+	}
+
+	defer listener.Close()
+
+	hostname, err := os.Hostname()
+	if err != nil {
+		fmt.Println("Error on getting hostname:", err)
+		os.Exit(1)
+	}
+
+	fmt.Println("Listening on - ", *addr)
+	for {
+		conn, err := listener.Accept()
+		if err != nil {
+			fmt.Println("Error accepting: ", err.Error())
+			os.Exit(1)
+		}
+
+		// Handle connections in a new goroutine.
+		go handleRequest(conn, hostname)
+	}
+}
+
+// Handles incoming requests.
+func handleRequest(conn net.Conn, reply string) {
+	fmt.Println("Handing connection from - ", conn.RemoteAddr())
+	conn.Write([]byte(reply))
+	conn.Close()
+}
